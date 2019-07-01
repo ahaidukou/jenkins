dac9c6a6bc0855ab49603134a6605755154224d7
Aleh Haidukou <aleh_haidukou@epam.com>
2019-07-01 16:08:43 +0300

    fixed all mistakes 7

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,3 @@
-
 """
 The function works as a summariser.
 
@@ -6,13 +5,10 @@ It takes 2 arguments and gives only one.
 """
 
 
-def calculator(x, y):
+def calculator(first_arg, second_arg):
     """Sum of two arguments."""
-    z = x + y
-    return z
+    the_sum = first_arg + second_arg
+    return the_sum
 
 
 print(calculator(1, 2))
-calculator(3, 5)
-calculator(4, 5)
-
